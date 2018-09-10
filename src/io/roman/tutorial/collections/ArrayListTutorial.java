57acecab3fa78da17c82b98d228f1b5622b759c2
Roman Gerteis <roman.gerteis@gmail.com>
2018-09-10 12:37:28 +0200

    renamed class

--- src/io/roman/tutorial/collections/ArrayListTutorial.java
+++ src/io/roman/tutorial/collections/ArrayListTutorial.java
@@ -1,14 +1,15 @@
 package io.roman.tutorial.collections;
+
 import java.util.*;
 
-public class StringArrayListTutorial {
-	
+public class ArrayListTutorial {
+
 	private ArrayList<String> myArraylist;
-	
+
 	public static void main(String args[]) {
-		
-		StringArrayListTutorial tutorial = new StringArrayListTutorial();
-		
+
+		ArrayListTutorial tutorial = new ArrayListTutorial();
+
 		tutorial.addValues();
 		tutorial.addValuesAtPosition();
 		tutorial.removeValues();
@@ -17,7 +18,7 @@ public class StringArrayListTutorial {
 		tutorial.iterateList();
 		tutorial.updateValue();
 		tutorial.listSize();
-		tutorial.createSubList(1,4);
+		tutorial.createSubList(1, 4);
 		tutorial.indexValues();
 		tutorial.copyToArray();
 		tutorial.compareTwoLists();
@@ -26,17 +27,17 @@ public class StringArrayListTutorial {
 		tutorial.combineTwoListsIntoOne();
 		tutorial.cloning();
 		tutorial.copyToStringArray();
-		
+
 	}
-	
+
 	private void copyToStringArray() {
 		// One method is to use the toArray function of the List Object
 		System.out.println("** ArrayList to String[] array ** ");
 		String values[] = myArraylist.toArray(new String[myArraylist.size()]);
-		for (String k:values) {
+		for (String k : values) {
 			System.out.println(k);
 		}
-		
+
 	}
 
 	private void cloning() {
@@ -44,15 +45,15 @@ public class StringArrayListTutorial {
 		// Use the clone method of Arraylist, but you are required to cast it
 		@SuppressWarnings("unchecked")
 		ArrayList<String> clone = (ArrayList<String>) myArraylist.clone();
-		
+
 		// now let's modify the clone and the original
 		myArraylist.set(3, "Janice");
 		myArraylist.remove("Rachel");
 		clone.add("Cole");
-		
+
 		System.out.println(myArraylist);
 		System.out.println(clone);
-		
+
 	}
 
 	private void combineTwoListsIntoOne() {
@@ -62,37 +63,36 @@ public class StringArrayListTutorial {
 		Collections.sort(newList);
 		System.out.println("** two combined arrays into one and then sorted **");
 		System.out.println(newList);
-		
+
 	}
 
 	private void swapElements() {
 		System.out.println("** Swapping two elements **");
 		System.out.println(myArraylist);
 		Collections.swap(myArraylist, 1, 3);
-		System.out.println(myArraylist);		
+		System.out.println(myArraylist);
 		Collections.swap(myArraylist, 1, 3);
 	}
 
 	private void compareTwoLists() {
 		@SuppressWarnings("unchecked")
 		ArrayList<String> anotherArrayList = (ArrayList<String>) myArraylist.clone();
-		
+
 		boolean eq;
-		
+
 		eq = myArraylist.equals(anotherArrayList);
 		System.out.println(eq);
-		
+
 		anotherArrayList.add("Rachel");
-		
+
 		eq = myArraylist.equals(anotherArrayList);
 		System.out.println(eq);
-		
+
 		anotherArrayList.remove("Rachel");
-		
+
 		// Another approach is to use array level comparison
 		eq = Arrays.equals(myArraylist.toArray(), anotherArrayList.toArray());
 		System.out.println(eq);
-		
 
 	}
 
@@ -100,119 +100,117 @@ public class StringArrayListTutorial {
 		// You can use collections static method to sort an ArrayList
 		Collections.sort(myArraylist);
 
-		// Or use the class method sort off the ArrayList Object 
+		// Or use the class method sort off the ArrayList Object
 		myArraylist.sort(null);
-		
+
 		System.out.println(myArraylist);
 	}
 
 	private void copyToArray() {
 		// getting an Object Array
 		Object[] oArr = myArraylist.toArray();
-		for(int i=0; i<oArr.length; i++) {
+		for (int i = 0; i < oArr.length; i++) {
 			System.out.println(oArr[i]);
 		}
-		
+
 		// get a String Array
 		String[] sArr = myArraylist.toArray(new String[myArraylist.size()]);
-		for(int i=0; i< sArr.length; i++) {
+		for (int i = 0; i < sArr.length; i++) {
 			System.out.println(sArr[i]);
-			
+
 		}
-		
-		
+
 	}
 
 	private void indexValues() {
 		myArraylist.add("Joey");
 		System.out.println(myArraylist);
-		System.out.println(myArraylist.indexOf("Joey"));		
+		System.out.println(myArraylist.indexOf("Joey"));
 		System.out.println(myArraylist.lastIndexOf("Joey"));
 		myArraylist.remove(myArraylist.lastIndexOf("Joey"));
-		
+
 	}
 
 	private void createSubList(int from, int to) {
 		// Create new ArrayList object
-		ArrayList<String> subList = new ArrayList<>(myArraylist.subList(from,to));
+		ArrayList<String> subList = new ArrayList<>(myArraylist.subList(from, to));
 		System.out.println(subList);
-		
+
 		// Retrieve the List object as return from ArrayList
-		List<String> otherList = myArraylist.subList(from, to);		
+		List<String> otherList = myArraylist.subList(from, to);
 		System.out.println(otherList);
-		
+
 	}
 
 	private void listSize() {
 		System.out.println(myArraylist.size());
-		
+
 	}
 
 	private void readValue() {
 		String val = myArraylist.get(3);
 		System.out.println(val);
-		
+
 	}
 
 	public void updateValue() {
 		myArraylist.set(2, "Joey");
-		for(String str:myArraylist) {
+		for (String str : myArraylist) {
 			System.out.println(str);
 		}
-		
+
 	}
 
 	public void iterateList() {
-		for (String str:myArraylist) {
+		for (String str : myArraylist) {
 			System.out.println(str);
 		}
-		
+
 	}
 
 	public void replaceValue(String oVal, String nVal) {
-		
-		if(myArraylist.indexOf(oVal) >= 0) {
+
+		if (myArraylist.indexOf(oVal) >= 0) {
 			myArraylist.remove(myArraylist.indexOf(oVal));
 			myArraylist.add(nVal);
 			System.out.println(this.myArraylist);
 		}
-		
+
 	}
 
 	public void removeValues() {
 		myArraylist.remove(3);
 		System.out.println(myArraylist);
-		
+
 	}
 
 	public void arrayOperations() {
-	
-		
+
 	}
 
 	public void addValuesAtPosition() {
 		this.myArraylist.add(3, "Chandler");
 		System.out.println(this.myArraylist);
-		
+
 	}
 
 	public void addValues() {
 		// Let's just add some values here
-		
+
 		this.myArraylist.add("Phoebe");
 		this.myArraylist.add("Rachel");
 		this.myArraylist.add("Joey");
 		this.myArraylist.add("Ross");
 		this.myArraylist.add("Monica");
-		
+
 		System.out.println(this.myArraylist);
-		
+
 	}
 
-	public StringArrayListTutorial() {
-		
+	public ArrayListTutorial() {
+
 		myArraylist = new ArrayList<>(50);
-		
+
 	}
-	
+
 }
